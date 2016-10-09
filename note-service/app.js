bfb15feeea7de887fce5af5cb65630d7bd501c5f
Cheong, Zhen Xiong Sean <sean21cheong@msn.com>
2016-10-09 09:15:46 +0800

    -Created microservice routes -Fixed an error where app.js could not run

--- note-service/app.js
+++ note-service/app.js
@@ -38,7 +38,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500).
+    res.status(err.status || 500)
     .json({
       status: 'error',
       message: err
@@ -49,7 +49,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500).
+  res.status(err.status || 500)
   .json({
     status: 'error',
     message: err.message
--- note-service/routes/index.js
+++ note-service/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+// postgres queries
+var db = require('../queries');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Note Service' });
 });
+
+// list all notes
+router.get('/api/notes', db.listNotes);
+
+// get single note
+router.get('/api/note', db.getNote);
+
+// create new note
+router.post('/api/note', db.createNote);
+
+// update single note
+router.put('/api/note', db.updateNote);
+
+// remove single note
+router.delete('/api/note', db.removeNote);
 
 module.exports = router;
