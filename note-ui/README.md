4e270906376bdd75e4a74844fc351a93da557198
Cheong, Zhen Xiong Sean <sean21cheong@msn.com>
2016-10-09 10:36:48 +0800

    created note-ui

---
+++ note-ui/README.md
@@ -0,0 +1,3 @@
+# Note-UI
+
+The user interface for Note-Application.
---
+++ note-ui/src/app/app.js
@@ -0,0 +1,23 @@
+angular.module( 'note-application', [
+  'templates-app',
+  'templates-common',
+  'note-application.home',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | Note Application' ;
+    }
+  });
+})
+
+;
