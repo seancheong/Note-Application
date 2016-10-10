b42a58be9311d81d00e5d56bc8ec623791df7a98
Cheong, Zhen Xiong Sean <sean21cheong@msn.com>
2016-10-10 19:54:23 +0800

    Added users table to notes.sql and modified notes table to have username column

--- note-service/schemas/notes.sql
+++ note-service/schemas/notes.sql
@@ -1,7 +1,17 @@
+DROP DATABASE IF EXISTS noteapp_db;
+CREATE DATABASE noteapp_db;
+
 \c noteapp_db;
 
 CREATE TABLE notes (
   subject VARCHAR PRIMARY KEY,
   content VARCHAR,
-  version INTEGER
+  version INTEGER,
+  username VARCHAR
+);
+
+CREATE TABLE users (
+  id SERIAL,
+  username VARCHAR PRIMARY KEY,
+  password VARCHAR
 );
